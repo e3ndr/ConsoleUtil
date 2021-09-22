7cf032317b9c99da267968fb18367ba41e9727d3
Ender <33337309+e3ndr@users.noreply.github.com>
2021-09-22 15:09:47 -0500

    Mark all StdStreams as nullable.

--- src/main/java/xyz/e3ndr/consoleutil/StdStreams.java
+++ src/main/java/xyz/e3ndr/consoleutil/StdStreams.java
@@ -3,14 +3,16 @@ package xyz.e3ndr.consoleutil;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+import org.jetbrains.annotations.Nullable;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class StdStreams {
     public static final StdStreams CURRENT = new StdStreams(System.out, System.err, System.in);
 
-    public final PrintStream out;
-    public final PrintStream err;
-    public final InputStream in;
+    public @Nullable final PrintStream out;
+    public @Nullable final PrintStream err;
+    public @Nullable final InputStream in;
 
 }
